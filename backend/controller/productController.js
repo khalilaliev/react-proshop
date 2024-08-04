917990f992344abb2a2849c0c7d8d856b601cae6
khalilaliev <khalilaliev0@gmail.com>
2024-08-04 18:58:40 +0200

    create product controller

---
+++ backend/controller/productController.js
@@ -0,0 +1,18 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  res.json(product);
+});
+
+export { getProducts, getProductById };
