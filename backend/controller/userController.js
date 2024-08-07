80feeb63963db568c35c8d835fd2e42efc84edf9
khalilaliev <khalilaliev0@gmail.com>
2024-08-07 20:44:33 +0200

    add json web token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,8 +1,34 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  console.log(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
