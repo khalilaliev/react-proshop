121d4880d4f6051134e6a54e1bf4cdbcde24686f
khalilaliev <khalilaliev0@gmail.com>
2024-08-07 17:51:10 +0200

    add user controller and user routes

---
+++ backend/controller/userController.js
@@ -0,0 +1,50 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+const authUser = asyncHandler(async (req, res) => {
+  res.send("auth user");
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update user profile");
+});
+
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user profile");
+});
+
+const getUserByID = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUser,
+  updateUserProfile,
+  getUserByID,
+  getUsers,
+  deleteUser,
+};
