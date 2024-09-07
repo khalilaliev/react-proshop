08051c787ab594a4033d9a882b38e2bec38aa3ea
khalilaliev <khalilaliev0@gmail.com>
2024-09-07 20:13:02 +0200

    fixed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { rollupOptions: { external: ["react-bootstrap", "react-icons"] } },
   server: {
     proxy: {
       // "/api": {
@@ -15,11 +14,6 @@ export default defineConfig({
       // "/uploads": "http://localhost:5000",
       "/api": "http://localhost:5001",
       "/uploads": "http://localhost:5001",
-    },
-  },
-  resolve: {
-    alias: {
-      "react-bootstrap": "node_modules/react-bootstrap",
     },
   },
   optimizeDeps: {
